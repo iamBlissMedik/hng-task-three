b1733fa8b0a51b06577c15ce682710f1d9bc03b9
Ellams Bliss <ellamsbliss@gmail.com>
2023-09-20 22:12:17 +1000

    logout import

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./assets/index.css";
 import { useAuth0 } from "@auth0/auth0-react";
 
 import LoginButton from "./components/LoginButton";
-import LogoutButton from "./components/LogOutButton";
+import LogoutButton from "./components/LogoutButton";
 import HomePage from "./components/HomePage";
 import Loading from "./components/Loading";
 
